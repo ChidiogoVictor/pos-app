749f820350ba93d39cc4e6cfc408dbea37c2e12d
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-03-16 22:42:22 +0100

    complete botton nav

--- pos-frontend/src/components/shared/BottomNav.jsx
+++ pos-frontend/src/components/shared/BottomNav.jsx
@@ -1,29 +1,30 @@
 import React from "react";
-import {
-  FaHome,
-  MdOutLineReorder,
-  MdTableBar,
-  CiCircleMore,
-} from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { MdOutlineReorder, MdTableBar } from "react-icons/md";
+import { CiCircleMore } from "react-icons/ci";
+import {BiSolidDish} from "react-icons/bi"
 
 const BottomNav = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-900 p-2 flex justify-around">
-      <button>
-        <FaHome className="inline mr-4" size={15} />
-        Home
+    <div className="fixed bottom-0 left-0 right-0 bg-gray-900 p-2 h-16 flex justify-around">
+      <button className="flex items-center justify-center text-white bg-gray-700 w-[150px] rounded-[20px]">
+        <FaHome className="inline mr-2 text-xl" />
+        <p>Home</p>
       </button>
-      <button>
-        <MdOutLineReorder className="inline mr-4" size={15} />
-        Orders
+      <button className="flex items-center justify-center text-white bg-gray-700 w-[150px] rounded-[20px]">
+        <MdOutlineReorder className="inline mr-2 text-xl" />
+        <p>Orders</p>
       </button>
-      <button>
-        <MdTableBar className="inline mr-4" size={15} />
-        Tables
+      <button className="flex items-center justify-center text-white bg-gray-700 w-[150px] rounded-[20px]">
+        <MdTableBar className="inline mr-2 text-xl" />
+        <p>Tables</p>
       </button>
-      <button>
-        <CiCircleMore className="inline mr-4" size={15} />
-        More
+      <button className="flex items-center justify-center text-white bg-gray-700 w-[150px] rounded-[20px]">
+        <CiCircleMore className="inline mr-2 text-xl" />
+        <p>More</p>
+      </button>
+      <button className="absolute bottom-9 bg-amber-300 text-white rounded-full p-3 items-center">
+        <BiSolidDish className="text-xl"/>
       </button>
     </div>
   );
