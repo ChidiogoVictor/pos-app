45eac16ab28255906d4b48eab3e04e4378b813d8
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-03-19 20:04:53 +0100

    Create PopularDishes component

---
+++ pos-frontend/src/components/home/PopularDishes.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { popularDishes } from "../../constants";
+
+const PopularDishes = () => {
+  return (
+    <div className="mt-6 pr-6">
+      <div className="bg-black w-full rounded-lg">
+        <div className="flex justify-between items-center px-6 py-4">
+          <h1 className="text-white text-lg font-semibold tracking-wide">
+            Popular Dishes
+          </h1>
+          <a href="" className="text-blue-700 text-sm font-semibold">
+            View all
+          </a>
+        </div>
+
+        <div className="overflow-y-scroll h-[580px]">
+          {popularDishes.map((dish) => {
+            return (
+              <div
+                key={dish.id}
+                className="flex items-center gap-4 bg-gray-800 rounded-[15px] px-6 py-4 my-4 mx-6"
+              >
+                <h1 className="text-white font-bold text-xl">
+                  {dish.id < 10 ? `0${dish.id}` : dish.id}
+                </h1>
+                <img
+                  src={dish.image}
+                  alt={dish.name}
+                  className="w-[50px] h-[50px] rounded-lg"
+                />
+                <div>
+                  <h1 className="text-white font-semibold">{dish.name}</h1>
+                  <p className="text-white text-sm font-semibold mt-1">
+                    <span className="text-gray-300">Orders: </span>
+                    {dish.numberOfOrders}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularDishes;
--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import MiniCard from "../components/home/MiniCard";
 import { BsCashCoin } from "react-icons/bs";
 import { GrInProgress } from "react-icons/gr";
 import RecentOrders from "../components/home/RecentOrders";
+import PopularDishes from "../components/home/PopularDishes";
 
 const Home = () => {
   return (
@@ -29,7 +30,9 @@ const Home = () => {
         <RecentOrders />
       </div>
       {/* Right Div */}
-      <div className="flex-[2] bg-blue-600"></div>
+      <div className="flex-[2]">
+        <PopularDishes />
+      </div>
       <BottomNav />
     </section>
   );
