29ccbe68b43a591b9e6100b5f47b99f2a50fd34a
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-03-26 18:21:47 +0100

    fixed overflow bug

--- pos-frontend/src/pages/Menu.jsx
+++ pos-frontend/src/pages/Menu.jsx
@@ -31,7 +31,41 @@ const Menu = () => {
         <MenuContainer />
       </div>
       {/* Right Div*/}
-      <div className="flex-1 bg-blue-800"></div>
+      <div className="flex-1 bg-black mt-4 mr-3 h-[calc(85vh-5rem)] rounded-lg pt-2">
+        {/* Customer Info*/}
+        <div className="flex items-center justify-between px-4 py-3">
+          <div className="flex flex-col items-start">
+            <h1 className="text-md text-white font-semibold tracking-wide">
+              Customer Name
+            </h1>
+            <p className="text-sm text-gray-400 font-medium mt-1">
+              # 101 / Naira
+            </p>
+            <p className="text-sm text-gray-400 font-medium mt-2">
+              January 19, 2025 05:34 PM
+            </p>
+          </div>
+          <button className="bg-yellow-500 p-3 text-1 font-bold rounded-lg">
+            CN
+          </button>
+        </div>
+        <hr className="border-gray-800 border-t-2" />
+        {/* Cart Items*/}
+        <div>
+          <h1>Order Details</h1>
+        </div>
+        <div className=" overflow-y-scroll h-[250px]">
+          <div className="bg-gray-900 rounded-lg px-4 py-2 mb-2">
+            <div className="flex items-center justify-between">
+              <h1 className="text-gray-400 font-semibold tracking-wide text-lg">
+                Chicken Tikka
+              </h1>
+              <p className="text-gray-400 font-semibold">x2</p>
+            </div>
+          </div>
+        </div>
+        {/* Bills */}
+      </div>
       <BottomNav />
     </section>
   );
